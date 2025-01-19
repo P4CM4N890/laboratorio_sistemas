1cc3e8755c71ce929e984c9ae1f88af3d1904e5c
P4CM4N890 <p4cm4n890@gmail.com>
2025-01-19 17:08:57 -0600

    Prueba deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://p4cm4n890.github.io/laboratorio_sistemas/",
+  base: "https://p4cm4n890.github.io/laboratorio_sistemas",
   build: {
     outDir: './docs'
   }
